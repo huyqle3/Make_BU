0592214d7af626a0308bce323bb129fa4384c622
huyle333 <huyle333@yahoo.com>
2014-03-22 05:48:53 -0400

    added updates to the memorybasketlist.js

--- js/memorybasketlist.js
+++ js/memorybasketlist.js
@@ -1,1 +1,16 @@
 console.log("Hello World");
+
+var completeURL = document.URL.split('=');
+var firstSplit = completeURL[3];
+var secondSplit = firstSplit.split('&');
+var exactInput = secondSplit[0];
+
+var camelSearchURL = "camelcamelcamel.com/search?sq=" + exactInput;
+
+//var desired = $("#products_list a.product_title:first");
+$(document).ready(function(){
+	var helloworld = $('#centerPlus').load(camelSearchURL + " .product_title");
+	console.log(helloworld);
+});
+//$("#foo").find("a.specialLink:first")    
+//$("<p>"+camelSearchURL+"</p>").insertAfter(".rsltL");
