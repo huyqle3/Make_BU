9d6ab14acea10dc2c9163db116c2f68dd171e16d
huyle333 <huyle333@yahoo.com>
2014-03-22 16:01:48 -0400

    fairly functional version

--- js/memorybasketitem.js
+++ js/memorybasketitem.js
@@ -20,14 +20,32 @@ var camelSearchURL = "http://camelcamelcamel.com/" + exactInput + "/product/" + 
 
 $(document).ready(function(){
 	test(camelSearchURL);
+	
+	$(".second_frame").hide();
+      
+        $(".main_tab").click(function(){
+          $(".main_frame").show();
+          $(".second_frame").hide();
+        });
+
+        $(".second_tab").click(function(){
+          $(".second_frame").show();
+          $(".main_frame").hide();
+	});
 });
 
 function test(link){
 	var selector = "#olpDivId";
-	$(selector).after('<a href="' + link +'"></a><iframe id="basket" src=' + link + 'style = "width:100%; height:100%; text-align:center"></iframe>');
-	$('#basket').css('background-color', 'white');
+	$(selector).after('<td><button type = "button"; style = "cursor:pointer;" class = "second_tab">' + 'Recent' + '</button></td>');
+	$(selector).after('<td><button type = "button"; style = "cursor:pointer;" class = "main_tab">' + 'All Time' + '</button></td>');
+	$(selector).after('<a href="' + link +'"></a><iframe class="second_frame" src='+link+'scrolling = "no" style="width:240px; height:180px; text-align:center; visibility: hidden; overflow: "hidden";></iframe>');
+	$(selector).after('<a href="' + link +'"></a><iframe class="main_frame" src='+link+'scrolling = "no" style="width:500px; height:230px; text-align:center; visibility: hidden; overflow: "hidden";></iframe>');
 	$(window).load(function(){  
-      	var filteredContents = $('#basket').contents().find('#section_amazon').html();
-      	$('#basket').contents().find('body').html(filteredContents);
+      	var filteredContents = $('.main_frame').contents().find('#section_amazon .yui3-g .pad').html();
+      	$('.main_frame').contents().find('body').html(filteredContents);
+      	$('.main_frame').css('visibility', 'visible');
+      	var filteredContents2 = $('.second_frame').contents().find('#section_amazon .yui3-g .history_list.product_pane').html();
+      	$('.second_frame').contents().find('body').html(filteredContents2);
+      	$('.second_frame').css('visibility', 'visible');
 	});
 }
