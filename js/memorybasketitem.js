65efb59a9a8cee506257af21081445c7266e6153
huyle333 <huyle333@yahoo.com>
2014-03-22 17:12:47 -0400

    final changes to Memory Basket

--- js/memorybasketitem.js
+++ js/memorybasketitem.js
@@ -3,7 +3,7 @@ var completeURL = document.URL.split('/');
 var exactInput = completeURL[3];
 var id = completeURL[5];
 var camelSearchURL = "http://camelcamelcamel.com/" + exactInput + "/product/" + id + "?context=browse";
-
+var camelChart = "http://charts.camelcamelcamel.com/us/" + id + "/amazon.png";
 //console.log(camelSearchURL);
 
 /*$(document).ready(function(){
@@ -20,24 +20,43 @@ var camelSearchURL = "http://camelcamelcamel.com/" + exactInput + "/product/" + 
 
 $(document).ready(function(){
 	test(camelSearchURL);
-	
 	$(".second_frame").hide();
+	$(".third_frame").hide();
       
         $(".main_tab").click(function(){
           $(".main_frame").show();
           $(".second_frame").hide();
+          $(".third_frame").hide();
         });
 
         $(".second_tab").click(function(){
           $(".second_frame").show();
           $(".main_frame").hide();
-	});
+          $(".third_frame").hide();
+		});
+        
+        $(".third_tab").click(function(){
+          $(".third_frame").show();
+          $(".main_frame").hide();
+          $(".second_frame").hide();
+		});
 });
 
 function test(link){
 	var selector = "#olpDivId";
+	if ($(selector).length){
+		
+	}else if($(".unified_widget.rcmBody").length){
+		selector = ".unified_widget.rcmBody";
+	}else{
+		selector = "#centerCol.centerColAlign";
+	}
+	//console.log(selector);
+	//console.log(camelChart);
+	$(selector).after('<td><button type = "button"; style = "cursor:pointer;" class = "third_tab">' + 'Graph' + '</button></td>');
 	$(selector).after('<td><button type = "button"; style = "cursor:pointer;" class = "second_tab">' + 'Recent' + '</button></td>');
 	$(selector).after('<td><button type = "button"; style = "cursor:pointer;" class = "main_tab">' + 'All Time' + '</button></td>');
+	$(selector).after('<img class="third_frame" src=' + camelChart + ' width = "600px" height = "300px" visibility = "hidden" overflow = "hidden">');
 	$(selector).after('<a href="' + link +'"></a><iframe class="second_frame" src='+link+'scrolling = "no" style="width:240px; height:180px; text-align:center; visibility: hidden; overflow: "hidden";></iframe>');
 	$(selector).after('<a href="' + link +'"></a><iframe class="main_frame" src='+link+'scrolling = "no" style="width:500px; height:230px; text-align:center; visibility: hidden; overflow: "hidden";></iframe>');
 	$(window).load(function(){  
@@ -47,5 +66,6 @@ function test(link){
       	var filteredContents2 = $('.second_frame').contents().find('#section_amazon .yui3-g .history_list.product_pane').html();
       	$('.second_frame').contents().find('body').html(filteredContents2);
       	$('.second_frame').css('visibility', 'visible');
+      	$('.third_frame').css('visibility', 'visible');
 	});
 }
